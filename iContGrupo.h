530f0061f0dc051b5cc4612eee6f9e648bb44851
estebanmenendez <e.menendez19@gmail.com>
2018-06-07 23:44:39 -0300

    Merge origin/master
    Conflicts:
    	ContProp.h
    	dist/Debug/GNU-Linux/pa2017

--- iContGrupo.h
+++ iContGrupo.h
@@ -14,6 +14,17 @@
 #ifndef ICONTGRUPO_H
 #define ICONTGRUPO_H
 
+#include"DtGrupo.h"
+#include"DtTipo.h"
+#include"DtContacto.h"
+#include "IKey.h"
+#include "Lista.h"
+#include "Usuario.h"
+#include "Grupo.h"
+#include "Grupo.h"
+
+using namespace std;
+
 class iContGrupo {
 public:
     iContGrupo();
@@ -21,6 +32,28 @@ public:
     virtual ~iContGrupo();
 private:
 
+virtual void create(DtGrupo,Usuario) = 0;
+virtual void crearTipoGrupo(DtTipo,Grupo) = 0;
+virtual void createTipo(DtTipo,Grupo) = 0;
+virtual void crearConvGrupo(Grupo) = 0;
+virtual void getUsuario(Usuario) = 0;
+virtual void getContactos() = 0;
+virtual void getDtContacto() = 0;
+virtual DtGrupo listarGrupos() = 0;
+virtual DtContacto seleccionarGrupo(string) = 0;
+virtual DtContacto seleccionarPart(int) = 0;
+virtual bool agregarNuevoAdmin(int) = 0;
+virtual DtContacto listarContactos() = 0;
+virtual DtContacto seleccionarContacto(int) = 0;
+virtual void agregarParticipante(DtContacto) = 0;
+virtual DtContacto listarParticipantes() = 0;
+virtual DtContacto seleccionarParticipante(int) = 0;
+virtual void eliminarPartipante(DtContacto) = 0;
+virtual void cancelar() = 0;
+virtual DtGrupo altaGrupo(string,string) = 0;
+virtual void Salir() = 0;
+
+    
 };
 
 #endif /* ICONTGRUPO_H */
