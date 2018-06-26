ae6a432917146588e5b5643bb903093eb2f83e34
Mauro <nvmslithar@gmail.com>
2018-06-26 14:57:06 -0300

    Merge Mauro-Mello

--- Mensaje.cpp
+++ Mensaje.cpp
@@ -88,7 +88,7 @@ DtMensaje * Mensaje::getMensaje() {
 }
 
 Lista * Mensaje::GetVistos() {
-    Lista *listaVistos;
+    Lista *listaVistos = new Lista();
     IIterator *it = visto->iterator();
     while (it->hasNext()) {
         Visto * vis = dynamic_cast<Visto*> (it->getCurrent());
