79e8f3fc8592c41f365d45946363069d61956309
Mauro <nvmslithar@gmail.com>
2018-06-28 14:44:54 -0300

    NI puta idea pero voy a reinstalar netbeans

--- main.cpp
+++ main.cpp
@@ -124,6 +124,7 @@ int main(int argc, char** argv) {
                         break;
                     default:
                         break;
+                       
                 }
             }
         } catch (invalid_argument& ia) {
